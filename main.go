44d800b262d522818f86e15776b9b6f446e0abcc
FlamesX-128 <glowingorangeflames@gmail.com>
2021-11-25 14:13:33 -0600

    First release

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+*/
 package main
 
 import (
@@ -33,11 +59,22 @@ func build(dir string, target string, name string, system System) {
 	}
 }
 
+func contains(list []string, subStr string) bool {
+	for _, str := range list {
+		if subStr == str {
+			return true
+
+		}
+	}
+
+	return false
+}
+
 func main() {
 	projectDir := flag.String("project-directory", "", "Path to the project")
 	binaryName := flag.String("binary-name", "main", "Binary name when building")
 	goruntines := flag.Int("max-goruntines", 3, "Maximum quantity of goroutines.")
-
+	onlyIn := flag.String("onlyIn", "", "Build only on these systems")
 	flag.Parse()
 
 	if projectDir == nil || *projectDir == "" {
@@ -49,6 +86,13 @@ func main() {
 		}
 
 		projectDir = &di
+	}
+
+	var onlyInSystems []string
+
+	if onlyIn != nil && *onlyIn != "" {
+		onlyInSystems = strings.Split(*onlyIn, " ")
+
 	}
 
 	os.RemoveAll(path.Join(*projectDir, "dist"))
@@ -103,6 +147,14 @@ func main() {
 	var i int
 
 	for _, system := range systems {
+		if (onlyInSystems != nil) && (len(onlyInSystems) > 0) {
+			if !contains(onlyInSystems, system.Name) {
+
+				continue
+			}
+
+		}
+
 		if i == *goruntines {
 			wg.Wait()
 			i = 0
